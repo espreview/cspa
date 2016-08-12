3c735f8cd5226f371251f02966755f31a493c408
esdesignpreview <es_git@eatsmart.co.jp>
2016-08-12 10:16:45 +0900

    SNSボタンの表示を修正

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require('gulp'),
 gulp.task('webserver', function() {
   gulp.src('sp') //Webサーバーで表示するサイトのルートディレクトリを指定
     .pipe(webserver({
+      host:'0.0.0.0',
       livereload: true, //ライブリロードを有効に
       //directoryListing: true //ディレクトリ一覧を表示するか。オプションもあり
     }));
