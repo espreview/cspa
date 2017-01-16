4df3ab1c6cd2c81862cf02a90c7cbab3dd70f599
esdesignpreview <es_git@eatsmart.co.jp>
2017-01-16 09:14:10 +0900

    2017/01/16　帯デザイン

---
+++ school.organizer.profile.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('.moreread').collapser({
+      mode: 'lines',
+      truncate: 2,
+      showText: '<img src="https://cookingschool.jp/image/sp/bg_arrow_b.png" width="30" alt="続きを読む">',
+      hideText: '<img src="https://cookingschool.jp/image/sp/bg_arrow_t.png" width="30" alt="閉じる">'
+    });
+});
